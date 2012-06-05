dd560a4b23bc2a6429e36e1a6e53bd4ae9d39d63
Darren Cauthon <darren@cauthon.com>
2012-06-04 22:49:49 -0500

    Make sure the block passed in is called as well.

--- lib/smoke_monster/view_model.rb
+++ lib/smoke_monster/view_model.rb
@@ -1,9 +1,12 @@
 module SmokeMonster
   class ViewModel
     def initialize(subject)
+      @subject = subject
     end
 
     def method_missing(meth, *args, &blk)
+      @subject.send(meth, *args, &blk)
+    rescue
       SmokeMonster::ViewModel.new nil
     end
   end
--- spec/smoke_monster/view_model_spec.rb
+++ spec/smoke_monster/view_model_spec.rb
@@ -28,4 +28,55 @@ describe SmokeMonster::ViewModel do
       first.object_id.wont_equal second.object_id
     end
   end
+
+  describe "when given a Person object" do
+    class Person
+      attr_accessor :first_name, :last_name
+    end
+
+    before do
+      @person = Person.new
+      @view_model = SmokeMonster::ViewModel.new @person
+    end
+
+    [["John", "Galt"], ["Howard", "Roark"]].each do |name|
+      describe "with name of #{name[0]} #{name[1]}" do
+        before do
+          @person.first_name = name[0]
+          @person.last_name = name[1]
+        end
+
+        it "should return John as the first name" do
+          @view_model.first_name.must_equal name[0]
+        end
+
+        it "should return Galt as the last name" do
+          @view_model.last_name.must_equal name[1]
+        end
+      end
+    end
+  end
+
+  describe "when given an object that uses a block" do
+    class Thing
+      class << self
+        attr_accessor :test_value
+      end
+      def test(&blk)
+        blk.call
+      end
+    end
+
+    before do
+      @thing = Thing.new
+      @view_model = SmokeMonster::ViewModel.new @thing
+    end
+
+    it "should call the block" do
+      @view_model.test do
+        Thing.test_value = true
+      end
+      Thing.test_value.must_equal true
+    end
+  end
 end
