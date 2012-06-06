71e089387dcc026d9bcde8be8e7e30cb423406c8
Darren Cauthon <darren@cauthon.com>
2012-06-06 18:32:41 -0500

    Refactoring.

--- lib/smoke_monster/array_to_object.rb
+++ lib/smoke_monster/array_to_object.rb
@@ -7,16 +7,26 @@ class Array
     data.each do |value|
       result = Object.new
       self.each_with_index do |property_name, index|
-        this_value = value
-        this_value = value[index] if value.kind_of?(Array)
-        result.instance_variable_set("@#{property_name}", this_value)
-        result.instance_eval("
-        class << self
-          attr_accessor :#{property_name}
-        end")
+        this_value = get_the_value(value, index)
+        add_reader_for(result, property_name, this_value)
       end
       results << result
     end
     results
+  end
+
+  private
+
+  def get_the_value(value, index)
+    return value unless value.kind_of?(Array)
+    value[index]
+  end
+
+  def add_reader_for(result, property_name, this_value)
+    result.instance_variable_set("@#{property_name}", this_value)
+    result.instance_eval("
+    class << self
+      attr_accessor :#{property_name}
+    end")
   end
 end
