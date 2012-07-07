5533389675f5d7473385f0a34748ba6e2ffa3b07
Darren Cauthon <darren@cauthon.com>
2012-07-07 14:40:55 -0500

    Refactor.

--- lib/smoke_monster/lazy_cover.rb
+++ lib/smoke_monster/lazy_cover.rb
@@ -6,7 +6,7 @@ module SmokeMonster
 
     alias :the_original_subject_from_the_base_class :the_original_subject
     def the_original_subject
-      @subject = @block.call if @subject.nil?
+      setup_the_subject
       the_original_subject_from_the_base_class
     end
 
@@ -14,9 +14,12 @@ module SmokeMonster
 
     alias :old_method_missing :method_missing
     def method_missing(meth, *args, &blk)
-      @subject = @block.call if @subject.nil?
+      setup_the_subject
       old_method_missing meth, *args, &blk
     end
 
+    def setup_the_subject
+      @subject = @block.call if @subject.nil?
+    end
   end
 end
