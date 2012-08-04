c0fc5b125b9c3d03497579ff6db59aaece8dc063
Darren Cauthon <darren@cauthon.com>
2012-08-03 23:21:22 -0500

    Commit file missing from previous commit.

---
+++ lib/subtle/array_to_objects.rb
@@ -0,0 +1,32 @@
+class Array
+
+  def to_objects(&blk)
+    records = blk.call
+    return [] if records.empty?
+    records.map { |record| create_object_for_this_record(record) }
+  end
+
+  private
+
+  def create_object_for_this_record(record)
+    result = Object.new
+    self.each_with_index do |property_name, index|
+      value = get_the_value(record, index)
+      add_reader_for(result, property_name, value)
+    end
+    result
+  end
+
+  def get_the_value(value, index)
+    return value unless value.kind_of?(Array)
+    value[index]
+  end
+
+  def add_reader_for(result, property_name, this_value)
+    result.instance_variable_set("@#{property_name}", this_value)
+    result.instance_eval("
+    class << self
+      attr_accessor :#{property_name}
+    end")
+  end
+end
