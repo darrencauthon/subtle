caaf6efac40e120d458394fb159996a157ea515f
Darren Cauthon <darren@cauthon.com>
2013-08-24 07:49:25 -0500

    Formatting.

--- lib/subtle.rb
+++ lib/subtle.rb
@@ -1,5 +1,5 @@
 require File.dirname(__FILE__) + '/subtle/cover.rb'
-Dir[File.dirname(__FILE__) + '/subtle/*.rb'].each {|file| require file }
+Dir[File.dirname(__FILE__) + '/subtle/*.rb'].each { |file| require file }
 
 module Subtle
 end
